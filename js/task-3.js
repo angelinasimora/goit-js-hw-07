e21a7eb99a0e7e4c2ed719a00aa14712c612ac88
Angelina Simora <angelinasimora997@gmail.com>
2024-11-03 23:13:48 +0100

    message

--- js/task-3.js
+++ js/task-3.js
@@ -1,42 +1,7 @@
-// Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, який записується у приватну властивість value об'єкта, що створюється.
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
 
-
-
-// Оголоси наступні методи класу:
-
-// getValue() — повертає поточне значення приватної властивості value.
-// padEnd(str) — отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта, який викликає цей метод.
-// padStart(str) — отримує параметр str (рядок) і додає його на початок значення приватної властивості value об'єкта, який викликає цей метод.
-// padBoth(str) — отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості value об'єкта, який викликає цей метод.
-
-
-class StringBuilder{
-    #value
-    constructor(initialValue) {
-        this.#value = initialValue;
-    }
-
-    getValue() {
-        return this.#value;
-    }
-    padEnd(str) {
-        this.#value = this.#value + str;
-    }
-    padStart(str) {
-        this.#value = str + this.#value;
-    }
-    padBoth(str) {
-        this.#value = str + this.#value + str;
-    }
-}
-
-
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+nameInput.addEventListener("input", () => {
+  const trimmedValue = nameInput.value.trim();
+  nameOutput.textContent = trimmedValue ? trimmedValue : "Anonymous";
+});
